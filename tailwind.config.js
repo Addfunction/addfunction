9636dcb0301f3d0fe5979eb24174edc8f23e0f73
Addfunction <134153077+Addfunction@users.noreply.github.com>
2025-08-27 18:00:24 +0200

    Add_Project.zip

---
+++ tailwind.config.js
@@ -0,0 +1,1 @@
+module.exports = { content: ['./src/**/*.{js,ts,jsx,tsx}'], theme: { extend: { colors: { neonBlue: '#00FFFF', neonPink: '#FF00FF', neonGreen: '#39FF14', darkBg: '#0A0A0A' } } }, plugins: [] }
